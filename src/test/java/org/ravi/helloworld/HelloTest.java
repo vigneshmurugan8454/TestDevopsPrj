bd0be3f7b2e678039d546b38705b75410702dbdc
Training_C2a.05.01 <vignesh.b.murugan@accenture.com>
2018-05-31 11:21:07 +0530

    Comment

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -19,7 +19,7 @@ public class HelloTest {
 	@BeforeTest
 	public void beforeTest() {
 		
-		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver","C:/Users/training_c2a.05.01/Downloads/chromedriver_win32 (2)/chromedriver.exe");
 		driver = new ChromeDriver();
 		driver.get("http://demoaut.com");
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
@@ -32,7 +32,9 @@ public class HelloTest {
 		String text1 = driver.findElement(By.xpath("//tr[@align = 'right']/td[@colspan = '2']/font[@size = '2' and @face = 'Arial, Helvetica, sans-serif, Verdana']/b")).getText();
 		System.out.println("The date is: "+text1);
 		driver.findElement(By.name("userName")).sendKeys("vishnu");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 		String text = driver.findElement(By.name("userName")).getAttribute("value");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
 		System.out.println("The Username entered is: "+text);
 
 	}
